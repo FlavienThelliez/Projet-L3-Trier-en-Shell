d643ea3e6155311879729bf5b6cff2d22b028457
Darkraiman <christopherdu62160@hotmail.fr>
2018-11-22 00:50:39 +0100

    Add files via upload

--- trishell.sh
+++ trishell.sh
@@ -1,58 +1,99 @@
 #!/bin/bash
+# /mnt/c/Users/christopher/desktop/projetShell/
 
-function tri_n(){
-    echo "Tri par nom"
-    find
-}
-function tri_s(){
-    echo "Tri par taille"
-}
-function tri_m(){
-    echo "Tri par date"
-}
-function tri_l(){
-    echo "Tri par nb lignes"
-}
-function tri_e(){
-    echo "Tri par extension"
-}
-function tri_t(){
-    echo "Tri par type de fichier"
-}
-function tri_p(){
-    echo "Tri par propriétaire"
-}
-function tri_g(){
-    echo "Tri par groupe propriétaire"
-}
+#Fonction dont on dispose dans ce projet pur l'instant
+
+#creeTab() qui prend en argument un repertoire et qui retourne une chaine
+#contenant le tableau et le nombre d'element de ce tableau séparé par un ;
 
 
-arg_r=false
-arg_d=false
+#extraitChaine() qui prend en argument une chaine issu de creeTab et qui renvoie le 
+#tableau associé
 
-while [ $# -gt 1 ]
-do
-    case $1 in
-	-R)
-	    echo "Tri des sous-répertoire"
-	    arg_r=true 
-	    ;;
-	-d)
-	    echo "Tri dans l'ordre décroissant"
-	    arg_d=true
-	    ;;
-	-*)
-	    echo "Autre option"
-	    echo "ajouter de quoi gérer la chaine"
-	    ;;
-	*)
-	    echo "Utilez ce format : ./trishell [-R] [-d] -[nsmletpg] rep"
-	    exit 1
-	    ;;
-    esac
-    shift
-done
+#extraitNbElement() qui prend en argument une chaine issu de creeTab et qui renvoie le 
+#nbElement associé
 
-rep=$1
-echo "Affichage du répertoire $rep"
+#getIndiceI() qui prend en argument une chaine S et indice I et qui retourne l'element 
+#à l'indice I du tableau S /!\ les indices vont de 1 à nbElement du tableau
 
+#parcoursTab() qui prend en argument une chaine S et un nombre d'element nbElem et qui
+#affiche tous les elements de S
+
+#swap() qui prend en argument une chaine S et deux indice I1 et I2, cette fonction
+#retourne une chaine similaire à S mais avec I1 et I2 inversé
+
+#identifieRep() qui prend en argument un tableau S (le tableau donné par creeTab) et
+#qui renvoie une chaine avec le meme fonctionnement que celle renvoyé par creeTab cad
+#un tableau et son nombre d'element séparé par un ;
+
+
+creeTab(){ #creeTab repertoire 
+    local rep=$1
+    local chaine=""
+    local nbElem=0
+    local listeFichier=`find $rep -maxdepth 1 | cut -c3-|grep -v '^$'`
+    for fichier in $listeFichier
+    do
+	chaine="$chaine#$fichier"
+	nbElem=$((nbElem+1))
+    done
+    chaine="$chaine#"
+    echo "$chaine;$nbElem"
+}
+
+getIndiceI(){ #getIndiceI chaine indice
+    local indice=$2
+    indice=$((indice+1))
+    echo $1 | cut -d# -f$indice
+}
+
+parcoursTab(){ #parcoursTab chaine tailleChaine
+    local indice=1
+    local element=""
+    while [ $indice -le $2 ]
+    do
+        element=`getIndiceI $1 $indice`
+	echo $element
+        indice=$((indice+1))
+    done
+}
+
+swap(){ #swap chaine ind1 ind2 (tailleTab potentiellement à voir)
+    local element1=`getIndiceI $1 $2`
+    local element2=`getIndiceI $1 $3`
+    local chaine=`echo $chaine | sed "s/#$element1#/###/" | sed "s/#$element2#/#$element1#/" | sed "s/###/#$element2#/"`
+    echo $chaine
+}
+
+extraitChaine(){
+    echo $1 | cut -d";" -f1
+}
+
+extraitNbElement(){
+    echo $1 | cut -d";" -f2
+}
+
+identifieRep(){
+    local reps=""
+    local fichiers=`parcoursTab $1 $2`
+    local nb=0
+    for element in $fichiers
+    do 
+	if [ -d $element ]
+	then 
+	    nb=$((nb+1))
+	    reps="$reps#$element"
+	fi
+    done
+    reps="$reps#;$nb"
+    echo $reps    
+}
+
+init=$1
+res=`creeTab $init`
+chaine=`extraitChaine $res`
+nbElem=`extraitNbElement $res`
+res=`identifieRep $chaine $nbElem`
+reps=`extraitChaine $res`
+nbReps=`extraitNbElement $res`
+parcoursTab $reps $nbReps
